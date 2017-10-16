9ac99d8080c86cb00024a815aa8209cefbdf2fbe
Maria Romanova <aemulare@users.noreply.github.com>
2017-10-16 00:55:12 -0400

    Modifies Polls controller.

--- controllers/polls.js
+++ controllers/polls.js
@@ -16,7 +16,7 @@ const PollsController = {
 
   // retrieves a list of all polls
   index(req, res) {
-    models.Poll.findAll()
+    db.polls.findAll()
     .then((allPolls) => {
       res.json(allPolls);
     });
@@ -24,28 +24,41 @@ const PollsController = {
 
   // creates a new poll object
   create(req, res) {
-    db.polls.create({
-    question: req.body.question
-  })
-  .then((poll) => {
-    res.json(poll);
-  })
-  // .catch(() => {
-  //     res.sendStatus(400);
-  //   });
-  },
+        db.polls.create({
+            question: req.body.question
+        })
+        .then((poll) => {
+            choices = req.body.choices;
+            for (let choice of choices) {
+                db.choices.create({
+                    poll_id: poll.id,
+                    description: choice
+                })
+                .then((ch) => {
+                });
+            }
+            return poll;
+        })
+        .then((poll) => {
+            res.json(poll)
+        })
+    },
 
   // retrieves poll with specific id
   show(req, res) {
-    models.Poll.findById(parseInt(req.params.id))
-    .then(poll => {
-      res.json(poll);
-    });
-  },
+        db.polls.findById(parseInt(req.params.id), {
+            include: [{
+                model: db.choices
+            }]
+        })
+        .then(poll => {
+          res.json(poll);
+        });
+    },
 
   // updates poll with specific id with new question
   update(req, res) {
-    models.Poll.findById(parseInt(req.params.id))
+    db.polls.findById(parseInt(req.params.id))
     .then(poll => {
       poll.updateAttributes({
         question: req.body.question
@@ -58,7 +71,7 @@ const PollsController = {
   
   // deletes poll with specific id
   delete(req, res) {
-    models.Poll.destroy({
+    db.polls.destroy({
       where: { id: parseInt(req.params.id) }
     })
     .then(poll => {
