a06f2525ca9f4c20e8677965705b27e40582d085
KevinCybura <KevinCybura@gmail.com>
2017-10-20 15:27:25 -0400

    completed

--- poll-app/controllers/polls.js
+++ poll-app/controllers/polls.js
@@ -3,62 +3,89 @@ const models = require('../models');
 
 const router = express.Router();
 
-
 // This route retrieves a list of all poll questions
 router.get('/', (req, res) => {
-  models.Polls.findAll()
-    .then((allPolls) => {
-      res.json(allPolls);
-    })
+  models.Polls.findAll().then(allPolls => {
+    res.json(allPolls);
+  });
 });
 
 // This route is for creating a new poll object
 //  We provide the `question` in the body parameters
 //  Note: this does NOT take an array of choices
 router.post('/', (req, res) => {
-  models.Polls.create({
-    question: req.body.question
-  })
-  .then((poll) => {
-    res.json(poll);
-  })
-  .catch(() => {
-    res.sendStatus(400);
-  })
+  models.Polls
+    .create({
+      question: req.body.question
+    })
+    .then(poll => {
+      res.json(poll);
+    })
+    .catch(() => {
+      res.sendStatus(400);
+    });
 });
 
 // This route is used to retrieve a specific poll object
 //  The query also retrieves all associated choices for the poll
 router.get('/:id', (req, res) => {
-  models.Polls.findById(parseInt(req.params.id), {
-    include: [{
-      model: models.Choices
-    }]
-  })
-  .then(poll => {
-    res.json(poll);
-  });
+  models.Polls
+    .findById(parseInt(req.params.id), {
+      include: [
+        {
+          model: models.Choices
+        }
+      ]
+    })
+    .then(poll => {
+      res.json(poll);
+    });
 });
 
 // This route is used for adding a choice for a specific poll
 //  The poll id is in the route parameters
 //  The choice description is in the parameters
 router.post('/:id/choices', (req, res) => {
-  models.Polls.findById(parseInt(req.params.id))
+  models.Polls
+    .findById(parseInt(req.params.id))
     .then(poll => {
-      models.Choices.create({
-        description: req.body.description,
-        PollId: poll.id
-      })
-      .then((choice) => {
-        res.json(choice);
-      })
+      models.Choices
+        .create({
+          description: req.body.description,
+          PollId: poll.id
+        })
+        .then(choice => {
+          res.json(choice);
+        });
     })
     .catch(() => {
-      console.log('error here')
+      console.log('error here');
       res.sendStatus(400);
     });
 });
-
-
+//changes question of poll
+router.put('/:id', (req, res) => {
+  models.Polls
+    .findById(parseInt(req.params.id))
+    .then(poll => {
+      poll.update({ question: req.body.question });
+      return poll;
+    })
+    .then((poll) => {
+      res.json(poll);
+    });
+});
+//deletes poll and all of its choices
+router.delete('/:id',(req,res) =>{
+  models.Polls
+  .findById(parseInt(req.params.id))
+  .then(poll => {
+    models.Choices.destroy({where:{PollId: poll.id}});
+    poll.destroy();
+    return poll
+  })
+  .then((poll) => {
+    res.json(poll);
+  });
+})
 module.exports = router;
