0f67408225886f70ff7d6fa39e6d598f44b99b07
KirstynNatavio <kirstynatavio@hotmail.com>
2017-10-20 19:21:18 -0400

    Implement route for put request

--- poll-app/controllers/polls.js
+++ poll-app/controllers/polls.js
@@ -55,10 +55,19 @@ router.post('/:id/choices', (req, res) => {
       })
     })
     .catch(() => {
-      console.log('error here')
+      console.log('error here');
       res.sendStatus(400);
     });
 });
 
+//To update poll question text
+router.put('/:id/', (req, res) => {
+  models.Polls.findById(parseInt(req.params.id)).then(poll => {
+    poll.update({question: req.body.question.toString(),})
+  })
+  .catch( () => {
+    console.log('error here');
+    res.sendStatus(400);
+  })
+})
 
-module.exports = router;
