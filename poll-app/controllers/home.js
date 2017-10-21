c643b32fc558694243b4b99339f945cbf41efbcf
Chuk Ho Wu <chukho.wu@gmail.com>
2017-10-20 22:12:08 -0400

    added PUT and DELETE methods

---
+++ poll-app/controllers/home.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const models = require('../models');
+
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+  res.json({
+    msg: "Successful GET to '/' route"
+  });
+});
+
+
+module.exports = router;
