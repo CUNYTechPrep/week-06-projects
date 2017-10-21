faf4fb6f1890ff53a9a754b9c800127393c63d29
Muhammad Umer <myemailum14@gmail.com>
2017-10-20 23:58:46 -0400

    Update app.js

--- poll-app/app.js
+++ poll-app/app.js
@@ -12,13 +12,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Uncomment the following if you want to serve up static assets.
 // (You must create the public folder)
-/*
+
 app.use(express.static('./public'));
-*/
+
 
 // Uncomment the following if you want to use handlebars
 // on the backend. (You must create the views folder)
-/*
+
 const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({
   layoutsDir: './views/layouts',
@@ -26,7 +26,7 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/views/`);
-*/
+
 
 
 
